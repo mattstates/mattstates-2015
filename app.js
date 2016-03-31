c4acac8bac3929b88a427c8f1d92f92a8be4a6a4
Matt States <mattstates@icloud.com>
2016-03-31 14:33:56 -0700

    This is for Heroku.

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+var pCod = require('./passcodes.js');
+//var database = require('./blogposts.js').database;
+var Blogpost = require('./blogposts.js').BlogPost;
 
-app.use(express.static('public'));
+///// USE THIS TO MANUALLY ADD POSTS TO THE DB ////
+var uploadAPost = new Blogpost({   
+                                    title: 'Video Poker 2015',
+                                    author: 'Matt States',
+                                    date: 'July 2015',
+                                    body: 'This was a game I wanted to program from the ground up usign HTML, CSS, and JavaScript without any libraries or frameworks. I did borrow some CSS for the background textures and the textures for the poker chips, but the rest was done without so much as a Stack Overflow search. I should go through the code base and refactor it and spruce up the design overall. My main goal was to make it look good on iOS and I think I accomplished that.',
+                                    exerpt: 'A video poker game in vanilla JavaScript.',
+                                    techused: ['HTML', 'CSS', 'JavaScript'],
+                                    siteURL: 'http://musicpants.net/webapps/video-poker/',
+                                    imageurl: '/assets/images/videopoker2015.png'
+                            });
+
+/*uploadAPost.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Post was uploaded to the database.');
+      };
+});*/
+///// END USE THIS TO MANUALLY ADD POSTS TO THE DB ////
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static(__dirname + '/public'));
 
 //Handle resquests and responses to the 'root'.
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    //res.sendFile(path.join(__dirname + 'index.html'));
+    res.render('index', {});
+});
+
+app.get('/api', function(req, res) {
+    
+    Blogpost.find({author: 'Matt States'}, function(err, posts) {
+        if (err) throw err;
+        res.send(posts);
+        console.log(posts);
+    });
+    
 });
 
 app.listen(3000, function() {
---
+++ blogposts.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var pCod = require('./passcodes.js');
+
+var blogpost = {
+    
+    //DATABASE CONNECTION
+    database: mongoose.connect('mongodb://'+pCod.mongoDB.username+':'+pCod.mongoDB.password+'@ds021299.mlab.com:21299/mattstates_homepage'),
+    
+    //BLOG POST SCHEMA
+    BlogPost: mongoose.model('BlogPost', {    
+                                    title: String,
+                                    author: String,
+                                    date: Date,
+                                    body: String,
+                                    exerpt: String,
+                                    techused: Array,
+                                    siteURL: String,
+                                    imageurl: String
+                                })
+    
+    //BLOG POST DATA TO UPLOAD
+    /*var testPost = new BlogPost({   
+                                    title: 'Test Post',
+                                    author: 'Test Author',
+                                    body: 'LOREM IPSUM BLAH',
+                                    techused: 'nodeJS'
+                                }),*/
+    
+    //SAVE TO THE DATABASE
+    /*testPost.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Post was uploaded to the database.');
+      };
+    });*/
+    
+};
+
+module.exports = blogpost;
